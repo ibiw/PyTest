9bd7c83f730c5e19221e62bfd54a92d10c9c2fdd
ibiw <ibiw@163.com>
2018-11-23 14:56:20 +0800

    class logger

---
+++ test1/logger.py
@@ -0,0 +1,50 @@
+import logging
+
+
+class Logger(object):
+    def __init__(self, file_name):
+        self.file = file_name
+        # create logger
+        self.logger = logging.getLogger()
+
+        # set default log level
+        self.logger.setLevel(logging.DEBUG)
+
+        # create console_handler and file_handler
+        self.console_handler = logging.StreamHandler()
+        self.file_handler = logging.FileHandler(self.file, 'w')
+
+        # create file formatter
+        self.file_formatter = logging.Formatter(
+            '%(asctime)s - %(name)s - %(levelname)s - %(filename)s:%(lineno)d  - %(message)s')
+        self.console_formatter = logging.Formatter('%(levelname)s - %(message)s')
+
+        # add formatter to handler
+        self.console_handler.setFormatter(self.console_formatter)
+        self.file_handler.setFormatter(self.file_formatter)
+
+        # add handlers to logger
+        self.logger.addHandler(self.console_handler)
+        self.logger.addHandler(self.file_handler)
+
+    def info(self, log_message):
+        self.logger.info(log_message)
+
+    def warning(self, log_message):
+        self.logger.warning(log_message)
+
+
+def main():
+    log_name = 'http_download.log'
+
+    l = Logger(log_name)
+
+    def test():
+        l.info('hello')
+        l.warning('warning')
+
+    test()
+
+
+if __name__ == "__main__":
+    main()
